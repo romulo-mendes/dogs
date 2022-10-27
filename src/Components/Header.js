a2fc7af79895b9ef8b99f9ad8293ae2b39232ab3
Rômulo Gomes <romulo.gomes.mendes@hotmail.com>
2022-10-27 18:00:36 -0300

    LoginPage

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,17 +2,27 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import { UserContext } from "../UserContext";
 
 const Header = () => {
+	const { data, userLogout } = React.useContext(UserContext);
+
 	return (
 		<header className={styles.header}>
 			<nav className={`${styles.nav} container`}>
 				<NavLink className={styles.logo} to="/" aria-label="Dogs - Home">
 					<Dogs />
 				</NavLink>
-				<NavLink className={styles.login} to="/login">
-					Login | Criar
-				</NavLink>
+				{data ? (
+					<NavLink className={styles.login} to="/conta">
+						{data.nome}
+						<button onClick={userLogout}>Sair</button>
+					</NavLink>
+				) : (
+					<NavLink className={styles.login} to="/login">
+						Login | Criar
+					</NavLink>
+				)}
 			</nav>
 		</header>
 	);
--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { USER_POST } from "../../api";
+import useForm from "../../Hooks/useForm";
+import Button from "../Form/Button";
+import Input from "../Form/Input";
 
 const LoginCreate = () => {
-  return (
-    <div>LoginCreate</div>
-  )
-}
+	const username = useForm();
+	const password = useForm("password");
+	const email = useForm("email");
 
-export default LoginCreate
+	async function handleSubmit(e) {
+		e.preventDefault();
+		const { url, options } = USER_POST({
+			username: username.value,
+			email: email.value,
+			password: password.value,
+		});
+		const response = await fetch();
+	}
+
+	return (
+		<section className="animeLeft">
+			<h1 className="title">Cadastre-se</h1>
+			<form onSubmit={handleSubmit}>
+				<Input label="Usuário" type="text" name="username" {...username} />
+				<Input label="Email" type="email" name="email" {...email} />
+				<Input label="Senha" type="password" name="password" {...password} />
+				<Button>Cadastrar</Button>
+			</form>
+		</section>
+	);
+};
+
+export default LoginCreate;
